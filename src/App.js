e08c9440ae134be3262911801d65b67b068bf79e
Vitalii Khymchuk <vitalii.workbox001@gmail.com>
2023-05-03 18:40:26 +0300

    React query in progress

--- src/App.js
+++ src/App.js
@@ -1,210 +1,210 @@
-import { useState, useEffect, useCallback } from 'react';
-import './App.css';
-import TaskForm from './components/TaskForm';
-import TaskList from './components/TaskList';
-import Header from './components/Header';
-import Notification from './components/PopUp';
-import { BeatLoader } from 'react-spinners';
+import { useState, useEffect, useCallback } from "react";
+import "./App.css";
+import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
+import Header from "./components/Header";
+import Notification from "./components/PopUp";
+import { BeatLoader } from "react-spinners";
 import {
-    getTasks,
-    createTask,
-    deleteTask,
-    updateTask,
-    markTaskAsImportant,
-    unmarkTaskAsImportant,
-    markTaskAsUrgent,
-    unmarkTaskAsUrgent,
-    markTaskAsCompleted,
-    unmarkTaskAsCompleted,
-} from './api/tasks';
+  getTasks,
+  createTask,
+  deleteTask,
+  updateTask,
+  markTaskAsImportant,
+  unmarkTaskAsImportant,
+  markTaskAsUrgent,
+  unmarkTaskAsUrgent,
+  markTaskAsCompleted,
+  unmarkTaskAsCompleted,
+} from "./api/tasks";
+import { useMutation, useQuery } from "react-query";
 
 function App() {
-    const [tasks, setTasks] = useState([]);
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(true);
+  // const [tasks, setTasks] = useState([]);
+  // const [error, setError] = useState(null);
+  // const [loading, setLoading] = useState(true);
 
-    const [message, setMessage] = useState('');
-    const [isTaskProcessing, setIsTaskProcessing] = useState(false);
-    const [timerId, setTimerId] = useState(null);
+  const [message, setMessage] = useState("");
+  const [isTaskProcessing, setIsTaskProcessing] = useState(false);
+  const [timerId, setTimerId] = useState(null);
 
-    const [page, setPage] = useState(1);
-    const [totalPages, setTotalPages] = useState(0);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
 
-    const fetchTasks = useCallback(async () => {
-        try {
-            setLoading(true);
-            const {
-                data: { data, total, per_page },
-            } = await getTasks(page, 15);
-            const totalPages = Math.ceil(Number(total) / Number(per_page));
-            setTasks(prevTasks => {
-                //checks if last item in tasks array === last item of array that we got to prevent of adding the same data
-                const shouldUpdate =
-                    prevTasks.length === 0 ||
-                    (data.length > 0 &&
-                        data[data.length - 1]?.id !==
-                            prevTasks[prevTasks.length - 1]?.id);
+  // const fetchTasks = useCallback(async () => {
+  //     try {
+  //         setLoading(true);
+  //         const {
+  //             data: { data, total, per_page },
+  //         } = await getTasks(page, 15);
+  //         const totalPages = Math.ceil(Number(total) / Number(per_page));
+  //         setTasks(prevTasks => {
+  //             //checks if last item in tasks array === last item of array that we got to prevent of adding the same data
+  //             const shouldUpdate =
+  //                 prevTasks.length === 0 ||
+  //                 (data.length > 0 &&
+  //                     data[data.length - 1]?.id !==
+  //                         prevTasks[prevTasks.length - 1]?.id);
 
-                return shouldUpdate ? [...prevTasks, ...data] : prevTasks;
-            });
+  //             return shouldUpdate ? [...prevTasks, ...data] : prevTasks;
+  //         });
 
-            setTotalPages(totalPages);
-            setLoading(false);
-        } catch (err) {
-            setError(err);
-            setLoading(false);
-        }
-    }, [page]);
+  //         setTotalPages(totalPages);
+  //         setLoading(false);
+  //     } catch (err) {
+  //         setError(err);
+  //         setLoading(false);
+  //     }
+  // }, [page]);
 
-    useEffect(() => {
-        fetchTasks();
-    }, [fetchTasks, page]);
+  const addTaskMutation = useMutation((task) => createTask(task));
 
-    if (loading) {
-        return (
-            <div className="loader-container">
-                <BeatLoader color="#aaa" size={40} />
-            </div>
-        );
+  const {
+    isLoading,
+    error,
+    data: tasks,
+  } = useQuery(
+    ["tasks", page],
+    () => {
+      getTasks(page, 15);
+    },
+    {
+      keepPreviousData: true,
     }
+  );
 
-    if (error) {
-        return <div>Error: {error.message}</div>;
+  const handleMarkImportant = async (taskId, isImportant) => {
+    if (isImportant) {
+      await markTaskAsImportant(taskId);
+    } else {
+      await unmarkTaskAsImportant(taskId);
     }
-
-    const handleMarkImportant = async (taskId, isImportant) => {
-        if (isImportant) {
-            await markTaskAsImportant(taskId);
-        } else {
-            await unmarkTaskAsImportant(taskId);
-        }
-        const updTasks = tasks.map(task =>
-            task.id === taskId ? { ...task, important: isImportant } : task,
-        );
-        setTasks(updTasks);
-
-        fetchTasks();
-    };
-
-    const handleMarkUrgent = async (taskId, isUrgent) => {
-        if (isUrgent) {
-            await markTaskAsUrgent(taskId);
-        } else {
-            await unmarkTaskAsUrgent(taskId);
-        }
-        const updTasks = tasks.map(task =>
-            task.id === taskId ? { ...task, urgent: isUrgent } : task,
-        );
-        setTasks(updTasks);
-
-        fetchTasks();
-    };
-
-    const handleMarkComplete = async (taskId, isComplete) => {
-        if (isTaskProcessing) return;
-
-        setIsTaskProcessing(true);
-
-        if (isComplete) {
-            setMessage('Task completed');
-            await markTaskAsCompleted(taskId);
-        } else {
-            await unmarkTaskAsCompleted(taskId);
-        }
-
-        setTimerId(
-            setTimeout(
-                async () => {
-                    const completed_at = isComplete ? Date.now() : null;
-                    const updTasks = tasks.map(task =>
-                        task.id === taskId ? { ...task, completed_at } : task,
-                    );
-                    setTasks(updTasks);
-                    setMessage('');
-                    fetchTasks();
-                    setIsTaskProcessing(false);
-                },
-                isComplete ? 4000 : 0,
-            ),
-        );
-    };
-
-    const handleDelete = async taskId => {
-        if (isTaskProcessing) return;
-        setIsTaskProcessing(true);
-        setMessage('Task deleted');
-
-        setTimerId(
-            setTimeout(async () => {
-                await deleteTask(taskId);
-                const updTasks = tasks.filter(({ id }) => id !== taskId);
-                setTasks(updTasks);
-                setMessage('');
-                fetchTasks();
-                setIsTaskProcessing(false);
-            }, 4000),
-        );
-    };
-
-    const clearTimeoutId = () => {
-        if (isTaskProcessing) return;
-        clearTimeout(timerId);
-        setMessage('');
-    };
-
-    const handleEdit = async (taskId, description) => {
-        await updateTask(taskId, { description });
-        const updTasks = tasks.map(task =>
-            task.id === taskId ? { ...task, description } : task,
-        );
-        setTasks(updTasks);
-    };
-
-    const handleAddTask = async description => {
-        const taskData = {
-            description,
-            created_at: new Date().toISOString(),
-            important: false,
-            urgent: false,
-        };
-        const { data } = await createTask(taskData);
-        setTasks(prevTasks => {
-            const updatedTasks = [data, ...prevTasks];
-            return updatedTasks;
-        });
-    };
-    const handleLoadMore = () => {
-        setPage(prevPage => prevPage + 1);
-    };
-
-    return (
-        <>
-            <Header />
-            <main>
-                <div className="g-row">
-                    {message && (
-                        <Notification text={message} onClick={clearTimeoutId} />
-                    )}
-
-                    <TaskForm onAddTask={handleAddTask} />
-                    <TaskList
-                        tasks={tasks}
-                        handleMarkImportant={handleMarkImportant}
-                        handleMarkUrgent={handleMarkUrgent}
-                        handleDelete={handleDelete}
-                        handleMarkComplete={handleMarkComplete}
-                        handleEdit={handleEdit}
-                        page={page}
-                    />
-                    {page < totalPages && (
-                        <button className="load_more" onClick={handleLoadMore}>
-                            Load More
-                        </button>
-                    )}
-                </div>
-            </main>
-        </>
+    const updTasks = tasks.map((task) =>
+      task.id === taskId ? { ...task, important: isImportant } : task
     );
+    setTasks(updTasks);
+
+    fetchTasks();
+  };
+
+  const handleMarkUrgent = async (taskId, isUrgent) => {
+    if (isUrgent) {
+      await markTaskAsUrgent(taskId);
+    } else {
+      await unmarkTaskAsUrgent(taskId);
+    }
+    const updTasks = tasks.map((task) =>
+      task.id === taskId ? { ...task, urgent: isUrgent } : task
+    );
+    setTasks(updTasks);
+
+    fetchTasks();
+  };
+
+  const handleMarkComplete = async (taskId, isComplete) => {
+    if (isTaskProcessing) return;
+
+    setIsTaskProcessing(true);
+
+    if (isComplete) {
+      setMessage("Task completed");
+      await markTaskAsCompleted(taskId);
+    } else {
+      await unmarkTaskAsCompleted(taskId);
+    }
+
+    setTimerId(
+      setTimeout(
+        async () => {
+          const completed_at = isComplete ? Date.now() : null;
+          const updTasks = tasks.map((task) =>
+            task.id === taskId ? { ...task, completed_at } : task
+          );
+          setTasks(updTasks);
+          setMessage("");
+          fetchTasks();
+          setIsTaskProcessing(false);
+        },
+        isComplete ? 4000 : 0
+      )
+    );
+  };
+
+  const handleDelete = async (taskId) => {
+    if (isTaskProcessing) return;
+    setIsTaskProcessing(true);
+    setMessage("Task deleted");
+
+    setTimerId(
+      setTimeout(async () => {
+        await deleteTask(taskId);
+        // const updTasks = tasks.filter(({ id }) => id !== taskId);
+        // setTasks(updTasks);
+        setMessage("");
+        // fetchTasks();
+        setIsTaskProcessing(false);
+      }, 4000)
+    );
+  };
+
+  const clearTimeoutId = () => {
+    if (isTaskProcessing) return;
+    clearTimeout(timerId);
+    setMessage("");
+  };
+
+  const handleEdit = async (taskId, description) => {
+    await updateTask(taskId, { description });
+    const updTasks = tasks.map((task) =>
+      task.id === taskId ? { ...task, description } : task
+    );
+    setTasks(updTasks);
+  };
+
+  const handleLoadMore = () => {
+    setPage((prevPage) => prevPage + 1);
+  };
+
+  const handleAddTask = (text) => addTaskMutation.mutate(text);
+
+  return (
+    <>
+      {isLoading && (
+        <div className="loader-container">
+          <BeatLoader color="#aaa" size={40} />
+        </div>
+      )}
+      {error && <div>Error: {error.message}</div>}
+      {!isLoading && !error && (
+        <>
+          <Header />
+          <main>
+            <div className="g-row">
+              {message && (
+                <Notification text={message} onClick={clearTimeoutId} />
+              )}
+
+              <TaskForm onAddTask={handleAddTask} />
+              <TaskList
+                tasks={tasks}
+                handleMarkImportant={handleMarkImportant}
+                handleMarkUrgent={handleMarkUrgent}
+                handleDelete={handleDelete}
+                handleMarkComplete={handleMarkComplete}
+                handleEdit={handleEdit}
+                page={page}
+              />
+              {page < totalPages && (
+                <button className="load_more" onClick={handleLoadMore}>
+                  Load More
+                </button>
+              )}
+            </div>
+          </main>
+        </>
+      )}
+    </>
+  );
 }
 
 export default App;
--- src/api/tasks.js
+++ src/api/tasks.js
@@ -1,75 +1,81 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_BASE_URL = 'https://task-matrix.online/api';
+const API_BASE_URL = "https://task-matrix.online/api";
 
 const api = axios.create({
-    baseURL: API_BASE_URL,
+  baseURL: API_BASE_URL,
 });
 
 const getTasks = (page, perPage) => {
-    return api.get(`/tasks?page=${page}&per_page=${perPage}`);
+  return api.get(`/tasks?page=${page}&per_page=${perPage}`);
 };
 
 const getCompletedTasks = (page, perPage) => {
-    return api.get(`/tasks/completed?page=${page}&per_page=${perPage}`);
+  return api.get(`/tasks/completed?page=${page}&per_page=${perPage}`);
 };
 
 const getTrashedTasks = (page, perPage) => {
-    return api.get(`/tasks/trashed?page=${page}&per_page=${perPage}`);
+  return api.get(`/tasks/trashed?page=${page}&per_page=${perPage}`);
 };
 
 const updateTask = (taskId, data) => {
-    return api.put(`/tasks/${taskId}`, data);
+  return api.put(`/tasks/${taskId}`, data);
 };
 
-const createTask = taskData => {
-    return api.post('/tasks', taskData);
+const createTask = (description) => {
+  const taskData = {
+    description,
+    created_at: new Date().toISOString(),
+    important: false,
+    urgent: false,
+  };
+  return api.post("/tasks", taskData);
 };
 
-const deleteTask = taskId => {
-    return api.delete(`/tasks/${taskId}`);
+const deleteTask = (taskId) => {
+  return api.delete(`/tasks/${taskId}`);
 };
 
-const markTaskAsUrgent = taskId => {
-    return api.post(`/tasks/${taskId}/urgent`);
+const markTaskAsUrgent = (taskId) => {
+  return api.post(`/tasks/${taskId}/urgent`);
 };
 
-const unmarkTaskAsUrgent = taskId => {
-    return api.post(`/tasks/${taskId}/not-urgent`);
+const unmarkTaskAsUrgent = (taskId) => {
+  return api.post(`/tasks/${taskId}/not-urgent`);
 };
 
-const markTaskAsImportant = taskId => {
-    return api.post(`/tasks/${taskId}/important`);
+const markTaskAsImportant = (taskId) => {
+  return api.post(`/tasks/${taskId}/important`);
 };
 
-const unmarkTaskAsImportant = taskId => {
-    return api.post(`/tasks/${taskId}/not-important`);
+const unmarkTaskAsImportant = (taskId) => {
+  return api.post(`/tasks/${taskId}/not-important`);
 };
 
-const markTaskAsCompleted = taskId => {
-    return api.post(`/tasks/${taskId}/complete`);
+const markTaskAsCompleted = (taskId) => {
+  return api.post(`/tasks/${taskId}/complete`);
 };
 
-const unmarkTaskAsCompleted = taskId => {
-    return api.post(`/tasks/${taskId}/not-complete`);
+const unmarkTaskAsCompleted = (taskId) => {
+  return api.post(`/tasks/${taskId}/not-complete`);
 };
 
-const restoreTask = taskId => {
-    return api.post(`/tasks/${taskId}/restore`);
+const restoreTask = (taskId) => {
+  return api.post(`/tasks/${taskId}/restore`);
 };
 
 export {
-    getTasks,
-    getCompletedTasks,
-    getTrashedTasks,
-    updateTask,
-    createTask,
-    deleteTask,
-    markTaskAsUrgent,
-    unmarkTaskAsUrgent,
-    markTaskAsImportant,
-    unmarkTaskAsImportant,
-    markTaskAsCompleted,
-    unmarkTaskAsCompleted,
-    restoreTask,
+  getTasks,
+  getCompletedTasks,
+  getTrashedTasks,
+  updateTask,
+  createTask,
+  deleteTask,
+  markTaskAsUrgent,
+  unmarkTaskAsUrgent,
+  markTaskAsImportant,
+  unmarkTaskAsImportant,
+  markTaskAsCompleted,
+  unmarkTaskAsCompleted,
+  restoreTask,
 };
